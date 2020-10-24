a24922fb15ef028f0a4d862323bf7cf013f26b94
Hiroki Shimizu <fortechandbusiness@gmail.com>
2020-10-24 16:52:11 +0900

    関数コピー, 値のみコピー

--- src/sampleCode/call_function.py
+++ src/sampleCode/call_function.py
@@ -1,6 +1,7 @@
 import openpyxl as xp
 
 wb = xp.load_workbook("kinou.xlsx")
+wb2 = xp.load_workbook("kinou.xlsx", data_only=True)
 
 
 def copy_and_paste(sheetname, num_min_row, num_max_row, num_min_column, num_max_column, sheetname_paste):
@@ -8,18 +9,39 @@ def copy_and_paste(sheetname, num_min_row, num_max_row, num_min_column, num_max_
     docstring
     """
     ws = wb[str(sheetname)]
+
     paste_ws = wb[sheetname_paste]
-    
+
     for j in range(num_min_column, num_max_column):
         for i in range(num_min_row, num_max_row):
             copy = ws.cell(row=i, column=j).value
             print("セル" + str(i) + "," + str(j) + ":" + str(copy))
-            
+            paste_ws.cell(row=i, column=j, value=copy)
+            i += 1
+        j += 1
+
+
+def copy_and_paste2(sheetname, num_min_row, num_max_row, num_min_column, num_max_column, sheetname_paste):
+    """
+    docstring
+    """
+    ws2 = wb2[str(sheetname)]
+
+    paste_ws = wb[sheetname_paste]
+
+    for j in range(num_min_column, num_max_column):
+        for i in range(num_min_row, num_max_row):
+            copy = ws2.cell(row=i, column=j).value
+            print("セル" + str(i) + "," + str(j) + ":" + str(copy))
+            paste_ws.cell(row=i, column=j, value=copy)
             i += 1
         j += 1
 
 
 #copy_and_paste("Sheet1", 3, 27, 2, 6)
 copy_and_paste("Sheet2", 1, 15, 1, 6, "Sheet3")
+
+new_ws = wb.create_sheet(title="Sheet4")
+copy_and_paste2("Sheet2", 1, 15, 1, 6, "Sheet4")
 
 wb.save("kinou3.xlsx")
